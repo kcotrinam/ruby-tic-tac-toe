43a3f4d7bfcaf54470ee6f92e0ad0abf25939817
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-22 11:10:02 -0500

    refactoring main structure

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,27 @@
 #!/usr/bin/env ruby
 
 # Ask for player names
-puts 'Welcome to our game: Tic Tac Toe'
-player1_name = ''
-player2_name = ''
+
 player1_choice = ''
 player2_choice = ''
 answer = ''
 game_on = true
 
-def game_confirmation(answer, player1_name, player2_name)
+def game_confirmation(answer)
   while (answer != 'y') && (answer != 'n')
     puts 'Do you want to play? (Y/N)'
     answer = gets.chomp.downcase
-    game_start(answer, player1_name, player2_name)
   end
+  (answer != 'y') && (answer != 'n') ? true : false
 end
 
+def request_players_info
+  print 'Enter the name of player 1: '
+  player1_name = gets.chomp
+  print 'Enter the name of player 2: '
+  player2_name = gets.chomp
+  return player1_name, player2_name
+end
 
 def ask_choice(player, player1_name, player2_name)
   if player == 1
@@ -42,84 +47,40 @@ def ask_choice(player, player1_name, player2_name)
   # display_board
 end
 
-
-# while game_on
-#   # loop for each move
-#      valid_choice = false
-#     # while valid_choice == false
-#     #   ask_choice
-      # the ask_choice function will return the choice
-      # if player == 1
-      #  player1_choice = ask_choice
-      # else
-      ## player2_choice = ask_choice
-#       end
-#   if winner # and/or draw (the exact condition in this milestone is not important)
-#     game_on = false
-#   end
-# end
-
-
-
-def request_players_info
+puts 'Welcome to our game: Tic Tac Toe'
+game_confirmation
+if game_confirmation
   print 'Enter the name of player 1: '
   player1_name = gets.chomp
   print 'Enter the name of player 2: '
   player2_name = gets.chomp
-
-  return player1_name, player2_name
+  # player = new player(player1_name)
+  # player = new player(player2_name)
+else
+  puts "good bye!..."
 end
 
-def game_start(answer, player1_name, player2_name)
-  if answer == 'y'
-    # Play the game
-    player1_name, player2_name = request_players_info
-    explain_game
-    # This will decide who is going to start
-    puts 'The player who will start the game will be chosen randomly'
-    puts 'I am thinking...'
-    sleep(3)
-    player = rand(1..2)
-    puts player == 1 ? "#{player1_name} will start, with X!" : "#{player2_name} will start, with O!"
-    # The game begins
-    puts 'The game begins...'
-    # We need to tell each player that it is his/her turn
-
-    ask_choice(player, player1_name, player2_name)
-
-    puts 'The final result is: '
-    display_board
-    puts 'The winner is...'
-    play_again(answer, player1_name, player2_name)
+while game_on
+# player_turn
+# display_board
+# player choice
+# # loop for each move
+  valid_choice = false
+  until valid_choice#(player choice)
+    
+  #  # valid_choice = true if ask_choice
+  end
+  if player == 1
+  #  player1_choice = ask_choice
   else
-    puts 'Goodbye!'
+  ## player2_choice = ask_choice
+  end
+#     
+  if winner # and/or draw
+    #   game_on = false
   end
 end
 
 
-
-def display_board
-  board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
-  board.length.times do |i|
-    board[i].length.times do |j|
-      if j == 2
-        puts board[i][j]
-      else
-        print board[i][j] + ' '
-      end
-    end
-  end
-  puts ''
-end
-
-def play_again(answer, player1_name, player2_name)
-  puts 'Do you want to play again? (Y/N)'
-  new_game = gets.chomp.downcase
-  if new_game == 'y'
-    game_start(answer, player1_name, player2_name)
-  else
-    p 'Good bye!'
-  end
-end
-
-game_confirmation(answer, player1_name, player2_name)
+puts 'the winner is...'
+# Game.winner
