c2387590327866f13807f6dcf3b9910e1db52714
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-22 15:52:33 -0500

    refactoring display game

--- bin/main.rb
+++ bin/main.rb
@@ -1,26 +1,18 @@
 #!/usr/bin/env ruby
 
 # Ask for player names
-
+puts 'Welcome to our game: Tic Tac Toe'
+player1_name = ''
+player2_name = ''
 player1_choice = ''
 player2_choice = ''
 answer = ''
-game_on = true
-
-def player_turn(player, player1_name, player2_name)
-  if player == 1
-    puts "It's your turn #{player1_name}. Make a move..."
-  else
-    puts "It's your turn #{player2_name}. Make a move..."
-  end
-end
-
-def game_confirmation(answer)
+def game_confirmation(answer, player1_name, player2_name)
   while (answer != 'y') && (answer != 'n')
     puts 'Do you want to play? (Y/N)'
     answer = gets.chomp.downcase
+    game_start(answer, player1_name, player2_name)
   end
-  answer == 'y'
 end
 
 def explain_game
@@ -36,74 +28,63 @@ def request_players_info
   player1_name = gets.chomp
   print 'Enter the name of player 2: '
   player2_name = gets.chomp
+  puts 'The player who will start the game will be chosen randomly'
+  puts 'I am thinking...'
+  sleep(3)
+  puts rand(1..2) == 1 ? "#{player1_name} will start, with X!" : "#{player2_name} will start, with O!"
   return player1_name, player2_name
 end
 
-def ask_choice(player, player1_name, player2_name)
-  if player == 1
-    puts "#{player1_name}, it is your turn. Choose a square"
-    player1_choice = gets.chomp
-    # We verify that the choice is valid
-    # verify_choice(player1_choice): it will return true or false
-    # if verify_choice(player1_choice) == true
-      # return player1_choice
-    # else
-      # we ask again
-  else
-    puts "#{player2_name}, it is your turn. Choose a square"
-    player2_choice = gets.chomp
-    # We verify that the choice is valid
-    # verify_choice(player2_choice): it will return true or false
-    # if verify_choice(player2_choice) == true
-      # return player2_choice
-    # else
-      # we ask again
-  end
-  # display_board
-end
-
-## Here, the game starts
-puts 'Welcome to our game: Tic Tac Toe'
-game_confirmation
-if game_confirmation # If it's true, we start the game
-  # Here, we request for player's information
+def game_start(answer, player1_name, player2_name)
+  counter = 1
   player1_name, player2_name = request_players_info
-  game_on = true
-  # Then, we create new objects (player1 and player2), with the given players' names
-  # player1 = new player(player1_name)
-  # player2 = new player(player2_name)
-
-else # If game confirmation returns false (that is: if the user chose 'n'), we say good bye!
-  puts 'good bye!...'
-  game_on = false
+  explain_game
+  puts 'The game begins...'
+  while answer == 'y'
+    if counter <= 3
+    # until (valid_choice(player1_choice) && (winning_move || drawing_move)) do      
+      puts "#{player1_name}, It's you turn choose a square"
+      player1_choice = gets.chomp
+    # end
+    display_board
+    # until (valid_choice(player1_choice) && (winning_move || drawing_move)) do
+    puts "#{player2_name}, It's you turn choose a square"
+    player2_choice = gets.chomp
+    # end
+    display_board
+      puts 'The final result is: '
+      display_board
+      puts 'The winner is...'
+      # play_again(answer, player1_name, player2_name)
+      counter += 1
+    else
+      break
+    end
+  end
 end
 
-explain_game
-player = rand(1..2)
-
-while game_on
-  valid_choice = false
-  # until validate_choice == true
-      # Tell player that it is his/her turn (call player_turn function)
-      # player_turn(player, player1_name, player2_name)
-      # display_board to show available moves
-      # Ask for player choice
-      # Validate the player's choice
-      # valid_choice = validate_choice(player_choice, player) returns true or false
-      # if valid_choice == true
-      #   switch the 'player' variable: player == 1 ? player = 2 : player = 1
-      #   make the move: modify_board(player, player_choice)
-      #   if winning_move ?
-            #  game_on = false
-            #  Display message saying that current player won the match
-            #  Congratulate player and get out of the program
-      #   elsif draw?
-            #  game_on = false
-            # Display message saying that it was a draw
-            # Congratulate players and get out of the program
-      #   else do nothing (game_on continues to be true)
-
-      # elsif (valid_choice == false)
-      #   Display an error, saying that it was an invalid choice, and ask again
-  # end
+def display_board
+  board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
+  board.length.times do |i|
+    board[i].length.times do |j|
+      if j == 2
+        puts board[i][j]
+      else
+        print board[i][j] + ' '
+      end
+    end
+  end
+  puts ''
 end
+
+def play_again(answer, player1_name, player2_name)
+  puts 'Do you want to play again? (Y/N)'
+  new_game = gets.chomp.downcase
+  if new_game == 'y'
+    game_start(answer, player1_name, player2_name)
+  else
+    p 'Good bye!'
+  end
+end
+
+game_confirmation(answer, player1_name, player2_name)
