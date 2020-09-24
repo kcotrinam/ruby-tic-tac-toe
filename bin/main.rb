541a414d1a5929388bdebba25882c8089a2bf95f
alvaro <aaruizf@unal.edu.co>
2020-09-24 10:30:41 -0500

    Refactor main game loop in main.rb and modified win? function in logic_game.rb

--- bin/main.rb
+++ bin/main.rb
@@ -15,7 +15,7 @@ end
 
 def explain_game
   puts "1. The game is played on a grid that's 3 squares by 3 squares."
-  puts '2. You are X, your friend is O.'
+  puts '2. Player 1 is X and player 2 is O.'
   puts '3. Players take turns putting their marks in empty squares.'
   puts '4. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.'
   puts '5. When all 9 squares are full, the game is over.'
@@ -33,11 +33,11 @@ def request_players_info(start)
   return player1_name, player2_name
 end
 
-def winning_move(player1_choice)
+def winning_move(player_choice)
   return false
 end
 
-def draw_move(player1_choice)
+def draw_move(player_choice)
   return false
 end
 
@@ -53,32 +53,50 @@ def game_start(answer, player1, player2, turn, new_board)
   player1_name = player1.player_name
   player2_name = player2.player_name
   while game_on
-    game_on = false unless counter <= 3
+    game_on = false unless counter <= 9
     if turn == 1
-      puts "#{player1_name}, It's you turn choose a square"
-      player1_choice = gets.chomp.to_i
-      if choice_validator(player1, player1_choice, new_board)
-        new_board.update(player1_choice, 'X')
+      puts "#{player1_name}, it's you turn choose a square"
+      player_choice = gets.chomp.to_i
+      if choice_validator(player1, player_choice, new_board)
+        new_board.update(player_choice, 'X')
+        if new_board.win?
+          puts "Hey, #{player1_name}, you won!!!"
+          return
+        elsif new_board.draw?
+          puts "Hey, #{player1_name}, it's a draw!!!"
+          return
+        end
         new_board.display
-        turn = 2
       else
-        while !choice_validator(player1, player1_choice, new_board)
+        while !choice_validator(player1, player_choice, new_board)
           puts "Not Correct!!!!!1 choose again"
-          player1_choice = gets.chomp.to_i
+          player_choice = gets.chomp.to_i
           new_board.display
         end
-        new_board.update(player1_choice, 'X')
-        # player1_choice = gets.chomp.to_i
+        new_board.update(player_choice, 'X')
         new_board.display
-        turn = 2
       end
+      turn = 2
     else
-      puts "#{player2_name}, It's you turn choose a square"
-      player2_choice = gets.chomp.to_i
-      choice_validator(player2, player2_choice, new_board)
-      if ((player1.sanitize_choice(player2_choice) && new_board.valid_cell?(player2_choice)) ? true : false)
-        puts choice_validator(player2, player2_choice, new_board)
-        new_board.update(player2_choice,'O')
+      puts "#{player2_name}, it's you turn choose a square"
+      player_choice = gets.chomp.to_i
+      if choice_validator(player2, player_choice, new_board)
+        new_board.update(player_choice,'O')
+        if new_board.win?
+          puts "Hey, #{player1_name}, you won!!!"
+          return
+        elsif new_board.draw?
+          puts "Hey, #{player1_name}, it's a draw!!!"
+          return
+        end
+        new_board.display
+      else
+        while !choice_validator(player2, player_choice, new_board)
+          puts "Not Correct!!!!! choose again"
+          player_choice = gets.chomp.to_i
+          new_board.display
+        end
+        new_board.update(player_choice, 'O')
         new_board.display
       end
       turn = 1
@@ -88,10 +106,10 @@ def game_start(answer, player1, player2, turn, new_board)
   puts 'The final result is: '
   new_board.display
   puts 'The winner is...'
-  play_again(answer, player1_name, player2_name)
+  play_again(answer, player1_name, player2_name, turn)
 end
 
-def play_again(answer, player1_name, player2_name)
+def play_again(answer, player1_name, player2_name, turn)
   puts 'Do you want to play again? (Y/N)'
   new_game = gets.chomp.downcase
   while new_game != 'y' && new_game != 'n'
